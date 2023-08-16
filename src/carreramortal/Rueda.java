31c6fc802722c172ffe78dbdc37ff6ae9ad94dfc
Julio Flores <juliofloresth@gmail.com>
2023-08-16 14:39:54 -0300

    Modificacion de Rueda

--- src/carreramortal/Rueda.java
+++ src/carreramortal/Rueda.java
@@ -3,12 +3,13 @@ package carreramortal;
 public class Rueda {
 
     private String marca;
-
     private double presion;
+    private int num;
 
-    public Rueda(String marca, double presion) {
+    public Rueda(String marca, double presion, int num) {
         this.marca=marca;
         this.presion=presion;
+        this.num=num;
     }
 
     public String getMarca() {
@@ -26,5 +27,26 @@ public class Rueda {
     public void setPresion(double presion) {
         this.presion = presion;
     }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
     
+    
+    public void inflarRueda(){
+        presion=28.0;
+        System.out.println("Rueda "+num+" inflada");
+    }
+    public void pincharRueda(){
+        presion=10;
+        System.out.println("Rueda "+num+" pinchada");
+    }
+    public void desinflarRueda(){
+        setPresion(getPresion()-0.5);
+        System.out.println("Rueda "+num+" desinflada, presion: "+getPresion());
+    }
 }
